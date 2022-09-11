4384f1e10f76c87d15f95f30c72d4d4addc3a8e5
Raphael Baere de Sá Oliveira <raphaelbaere@id.uff.br>
2022-09-11 20:15:27 -0300

    Add files via upload

---
+++ maria-project/script.js
@@ -0,0 +1,16 @@
+const estilos = [
+    'Casual',
+    'Fancy',
+    'Confortable',
+    'Cute',
+    'Baggy', 
+    'Formal']
+
+    const button = document.getElementById('cloth-picker-button');
+    const section = document.getElementById('cloth-picker-chose')
+    const p = document.getElementById('style-chosen')
+
+    button.addEventListener('click', () => {
+        const estiloEscolhido = estilos[Math.floor(Math.random() * estilos.length)]
+        p.innerHTML = estiloEscolhido;
+        })
