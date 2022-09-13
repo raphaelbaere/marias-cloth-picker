83fb61db7b1ed7e18d49e4fd0c46fe4dde7c986c
raphaelbaere <raphaelbaere@id.uff.br>
2022-09-13 14:54:16 -0300

    Few more steps

--- maria-project/scriptArmario.js
+++ maria-project/scriptArmario.js
@@ -1,5 +1,8 @@
 const closetTitleContainer = document.querySelector('#closet-title-container');
 const clotheItemContainer = document.querySelector('#clothe-items-container')
+const buttonNames = ['Casual', 'Formal', 'Confortable', 'Baggy', 'Cute', 'Fancy', 'All'];
+const aside = document.querySelector('#button-aside');
+const closetItems = document.querySelectorAll('.style-items')
 
 function createClosetElement(closetObj) {
     const h2 = document.createElement('h2');
@@ -24,6 +27,74 @@ function loadClothes() {
 }
 }
 
+function loadStyle(styleName) {
+    if (localStorage.getItem(styleName)) {
+    const styleObj = JSON.parse(localStorage.getItem(styleName));
+    for (let index = 0; index < styleObj.length; index += 1) {
+        const closetElement = createClosetElement(styleObj[index]);
+        clotheItemContainer.appendChild(closetElement);
+        }
+    } else {
+        removeClothes();
+        const p = document.createElement('p');
+        p.innerHTML = "You don't have clothes of this style in your closet yet. Go add some!";
+        p.id = 'closet-title';
+        clotheItemContainer.appendChild(p);
+        const button = document.createElement('a');
+        button.id = 'closet-button';
+        button.href = "/maria-project/salvarRoupa.html";
+        button.innerHTML = "Let's add some";
+        clotheItemContainer.appendChild(button);
+    }
+}
+
+function removeClothes () {
+clotheItemContainer.innerHTML = '';
+}
+
+function loadButtons() {
+    for (let index = 0; index < buttonNames.length; index += 1) {
+        const createButton = document.createElement('button');
+        createButton.innerHTML = buttonNames[index];
+        createButton.classList.add('closetButtons');
+        createButton.addEventListener('click', (event) => {
+            switch (event.target.innerHTML) {
+                case 'Casual':
+                    removeClothes();
+                    loadStyle('Casual');
+                break;   
+                case 'Formal':
+                    removeClothes();
+                    loadStyle('Formal');
+                break;   
+                case 'Baggy':
+                    removeClothes();
+                    loadStyle('Baggy');
+                break;         
+                case 'Confortable':
+                    removeClothes();
+                    loadStyle('Confortable');
+                break;
+                case 'Cute':
+                    removeClothes();
+                    loadStyle('Cute');
+                break;             
+                case 'Fancy':
+                    removeClothes();
+                    loadStyle('Fancy');
+                break;                
+                case 'All':
+                    removeClothes();
+                    loadStyle('Closet');
+                break;            
+            }
+
+        })
+        aside.appendChild(createButton);
+
+    }
+}
+
 function onload() {
     if (!localStorage.getItem('Closet')) {
         const p = document.createElement('p');
@@ -36,7 +107,8 @@ function onload() {
         button.innerHTML = "Let's add some";
         closetTitleContainer.appendChild(button);
     } else {
-        loadClothes()
+        loadButtons();
+        loadClothes();
     }
 }
 window.onload = onload();
--- maria-project/scriptSalvar.js
+++ maria-project/scriptSalvar.js
@@ -10,6 +10,12 @@ function submitNewClothes(event) {
     event.preventDefault();
 
     const closet = JSON.parse(localStorage.getItem('Closet')) || [];
+    const casual = JSON.parse(localStorage.getItem('Casual')) || [];
+    const formal = JSON.parse(localStorage.getItem('Formal')) || [];
+    const baggy = JSON.parse(localStorage.getItem('Baggy')) || [];
+    const confortable = JSON.parse(localStorage.getItem('Confortable')) || [];
+    const cute = JSON.parse(localStorage.getItem('Cute')) || [];
+    const fancy = JSON.parse(localStorage.getItem('Fancy')) || [];
 
     const newClothes = {
         clothesStyle: clothesStyle.value,
@@ -19,6 +25,32 @@ function submitNewClothes(event) {
     }
 
     closet.push(newClothes);
+
+    switch (newClothes.clothesStyle) {
+        case 'casual':
+            casual.push(newClothes);
+            localStorage.setItem('Casual', JSON.stringify(casual));
+        break;
+        case 'formal':
+            formal.push(newClothes);
+            localStorage.setItem('Formal', JSON.stringify(formal));
+        break;
+        case 'baggy':
+            baggy.push(newClothes);
+            localStorage.setItem('Baggy', JSON.stringify(baggy));
+        break;
+        case 'confortable':
+            confortable.push(newClothes);
+            localStorage.setItem('Confortable', JSON.stringify(confortable));
+        break;
+        case 'cute':
+            cute.push(newClothes);
+            localStorage.setItem('Cute', JSON.stringify(cute));
+        break;
+        case 'fancy':
+            fancy.push(newClothes);
+            localStorage.setItem('Fancy', JSON.stringify(fancy));
+    }
 
     localStorage.setItem('Closet', JSON.stringify(closet));
 }
